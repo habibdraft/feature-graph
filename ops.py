8bca369feee4b210c036ffdbf8affbaf1ced05f3
Nazia Habib <habib.nazia@gmail.com>
2026-06-22 10:45:28 -0500

    Create ops.py

---
+++ ops.py
@@ -0,0 +1,12 @@
+import torch
+
+def enter(mask):
+    x = mask.int()
+    return torch.cat([torch.tensor([False]), x.diff() == 1])
+
+def exit(mask):
+    x = mask.int()
+    return torch.cat([torch.tensor([False]), x.diff() == -1])
+
+def between(start, end):
+    return (start.cumsum(dim=0) - end.int().cumsum(dim=0)) > 0
