c7868631c2cb4f760f1064ef3114e4325d04d0ce
Nazia.Habib <nazia.habib@patenergy.com>
2026-06-23 09:00:02 -0500

    add feature graph level 1

---
+++ example.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+import torch
+
+from feature_graph.features import get_base_features, get_feature_graph
+from feature_graph.project_constants import baseline_df_file, df_file, axes
+from feature_graph.operators import enter, exit, between, operators
+
+baseline_df = pd.read_csv(baseline_df_file)
+df = pd.read_csv(df_file, nrows=500000)
+
+df = df.rename(columns=axes)
+df = df[axes.values()]
+
+base_features = get_base_features(axes, df, 'fault_number')
+feature_graph = get_feature_graph(base_features, operators)
